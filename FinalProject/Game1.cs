7fd0a384eacee37b9204e123179ce900cb8db7ab
Raihan Carder <339509226@GoTVDSB.ca>
2023-12-18 10:56:50 -0500

    how gravity?

--- FinalProject/Game1.cs
+++ FinalProject/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace FinalProject
 {
@@ -8,10 +9,14 @@ namespace FinalProject
     {
         // Final Project
         // Raihan Carder
-
+        List<Rectangle> barriers;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
-
+        KeyboardState keyboardState;
+        Player stickman;
+        Texture2D testingTexture;
+        Texture2D wallTexture;
+        bool jumping;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -26,23 +31,56 @@ namespace FinalProject
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            
             base.Initialize();
+            stickman = new Player(testingTexture, 10, _graphics.PreferredBackBufferHeight-35);
+            barriers = new List<Rectangle>();
+            jumping = false;
+            for (int i = 0; i < 1; i++)
+            {
+                barriers.Add(new Rectangle(0,495,1100,50));
+            }
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            testingTexture = Content.Load<Texture2D>("rectangle");
+            wallTexture = Content.Load<Texture2D>("rectangle");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
+
+            keyboardState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
             // TODO: Add your update logic here
+            stickman.HSpeed = 0;
+            stickman.VSpeed = 0;
+
+            if (keyboardState.IsKeyDown(Keys.D))         
+                stickman.HSpeed = 3;            
+            else if (keyboardState.IsKeyDown(Keys.A))
+                stickman.HSpeed = -3;
+            //if (keyboardState.IsKeyDown(Keys.W))
+            //    stickman.VSpeed = -3;
+            //else if (keyboardState.IsKeyDown(Keys.S))
+            //    stickman.VSpeed = 3;
+
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                jumping = true;
+            }
+
+            stickman.Update();
+
+            foreach (Rectangle barrier in barriers)
+                if (stickman.Collide(barrier))
+                    stickman.UndoMove();
 
             base.Update(gameTime);
         }
@@ -50,6 +88,14 @@ namespace FinalProject
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            stickman.Draw(_spriteBatch);
+            foreach (Rectangle barrier in barriers)
+                _spriteBatch.Draw(wallTexture, barrier, Color.Black);
+            _spriteBatch.End();
+
 
             // TODO: Add your drawing code here
 
---
+++ FinalProject/Player.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    class Player
+    {
+        private Texture2D _texture;
+        private Rectangle _location;
+        private Vector2 _speed;
+
+        public Player(Texture2D texture, int x, int y)
+        {
+            _texture = texture;
+            _location = new Rectangle(x, y, 30, 30);
+            _speed = new Vector2();
+        }
+        public float HSpeed
+        {
+            get { return _speed.X; }
+            set { _speed.X = value; }
+        }
+        public float VSpeed
+        {
+            get { return _speed.Y; }
+            set { _speed.Y = value; }
+        }
+        private void Move()
+        {
+            _location.X += (int)_speed.X;
+            _location.Y += (int)_speed.Y;
+        }
+        public bool Collide(Rectangle item)
+        {
+            return _location.Intersects(item);
+        }
+        public void UndoMove()
+        {
+            _location.X -= (int)_speed.X;
+            _location.Y -= (int)_speed.Y;
+        }
+        public void Update()
+        {
+            Move();
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _location, Color.White);
+        }
+    }
+ 
+}
